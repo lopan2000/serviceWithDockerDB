6aa6c6d5537f9c5c965f393391a52aae74144ecb
computer <velhalen@gmail.com>
2024-11-16 17:05:57 -0500

    commit

---
+++ src/test/java/com/jlevin/servicetodockerdb/myservice/MyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlevin.servicetodockerdb.myservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
